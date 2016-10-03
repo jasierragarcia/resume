6c9c1b3da82c0c7fb57549d2390483c0c0a41875
Juan Sierra <jasierra.garcia@gmail.com>
2016-10-03 11:27:18 -0400

    Add objects with info data

---
+++ js/resumeBuilder.js
@@ -0,0 +1,108 @@
+var bio = {
+  "name" : "Juan Sierra",
+  "role" : "Front-End Web Developer",
+  "contacts" : {
+  "mobile" : "571-271-5709",
+  "email" : "jasierra.garcia@gmail.com",
+  "github" : "https://github.com/jasierragarcia",
+  "linkedin" : "https://www.linkedin.com/in/juan-a-sierra-g-368b84100",
+  "twitter" : "@jasierra.garcia",
+  "location" : "Loundoun, VA"
+  },
+  "welcomeMessage" : "Thank you for visiting!",
+  "skills" : ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery", "Git"],
+  "biopic" : "images/me.png"
+};
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Northern Virginia Community College",
+      "location" : "Sterling, VA",
+      "degree" : "",
+      "majors" : "Computer Science",
+      "dates" : "August 2013 - December 2014",
+      "url" : "https://www.nvcc.edu/"
+    },
+    {
+      "name" : "Tuscarora High School",
+      "location" : "Leesburg, VA",
+      "degree" : "GED",
+      "majors" : "",
+      "dates" : "August 2010 - June 2013",
+      "url" : "https://www.nvcc.edu/"
+    },
+    {
+      "name" : "Heritage High School",
+      "location" : "Leesburg, VA",
+      "degree" : "",
+      "majors" : "",
+      "dates" : "August 2009 - June 2010",
+      "url" : "http://www.loudoun.k12.va.us/hhs"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Front-End Web Development Nanodegree",
+      "school" : "Udacity",
+      "dates" : "July 2016 - Present",
+      "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    },
+    {
+      "title" : "Full Stack Web Development",
+      "school" : "FreeCodeCamp",
+      "dates" : "July 2016 - Present",
+      "url" : "https://www.freecodecamp.com/"
+    }
+  ]
+};
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "Trapollo",
+      "title" : "Tech Support / Customer Service Representative",
+      "location" : "Sterling, VA",
+      "dates" : "March 2016 - Present",
+      "description" : "Strategically develop troubleshooting solutions for clients experiencing technical issues, system failures, and malfunctions."
+    },
+    {
+      "employer" : "Chief-Technologies",
+      "title" : "Intern Junior Engineer / Contractor",
+      "location" : "Leesburg, VA",
+      "dates" : "June 2013 - Present",
+      "description" : "Collaborates with technology and engineering professionals to contribute to creation, development, and implementation of complex hardware and software systems. Branch out to contractor jobs, currently working as a Tech Support / Customer Service Rep. at Trapollo."
+    },
+    {
+      "employer" : "FedBiz IT Solutions",
+      "title" : "Inside Sales Representative",
+      "location" : "Leesburg, VA",
+      "dates" : "August 2015 - February 2016",
+      "description" : "Undergo sales/technical training to be able to quote new hardware or software to government agencies."
+    },
+    {
+      "employer" : "Zenius Corporation",
+      "title" : "Junior Analyst",
+      "location" : "Leesburg, VA",
+      "dates" : "September 2015 - February 2016",
+      "description" : "Update company data base with candidates' resumes for Government Job Requests."
+    }
+  ]
+};
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "Portfolio Site Project",
+      "dates" : "September 2016",
+      "description" : "From mockup design, to coded website.",
+      "images" : "images/portfolio-project.png"
+    },
+    {
+      "title" : "Animal Trading Cards",
+      "dates" : "July 2016",
+      "description" : "Learning HTML and CSS.",
+      "images" : "images/card.png"
+    }
+  ]
+};
