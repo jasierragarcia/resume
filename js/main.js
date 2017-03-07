494d8bf3fd9990a26124cbd6c2f3a0b68f87f2de
Juan Sierra <jasierra.garcia@gmail.com>
2017-03-07 14:22:08 -0500

    Working on appending online courses data to index html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var bio = {
-    "name" : "Juan Sierra",
-    "role" : "Front End Developer",
-    "contacts" : {
-        "mobile" : "571-271-5709",
-        "email" : "jasierra.garcia@gmail.com",
-        "github" : "juan-sierra",
-        "twitter" : "jasierragarcia",
-        "location" : "Leesburg, VA"
+    "name": "Juan Sierra",
+    "role": "Front End Developer",
+    "contacts": {
+        "mobile": "571-271-5709",
+        "email": "jasierra.garcia@gmail.com",
+        "github": "juan-sierra",
+        "twitter": "jasierragarcia",
+        "location": "Leesburg, VA"
     },
-    "welcomeMessage" : "Thank you for visiting my website",
-    "skills" : [
+    "welcomeMessage": "Thank you for visiting my website",
+    "skills": [
         "HTML",
         "CSS",
         "JavaScript",
@@ -17,89 +17,85 @@ var bio = {
         "Linux",
         "Bash / ZSH"
     ],
-    "biopic" : "images/me.jpg"
+    "biopic": "images/me.jpg"
 };
 
 var education = {
-    "schools" : [
-        {
-            "name" : "Strayer University",
-            "location" : "Washington, D.C.",
-            "degree" : "B.S.",
-            "majors" : "Computer Science",
-            "dates" : "January 2017 - Present",
-            "url" : "http://www.strayer.edu/"
+    "schools": [{
+            "name": "Strayer University",
+            "location": "Washington, D.C.",
+            "degree": "B.S.",
+            "majors": "Computer Science",
+            "dates": "January 2017 - Present",
+            "url": "http://www.strayer.edu/"
         },
         {
-           "name" : "Tuscarora High School",
-            "location" : "Leesburg, VA",
-            "degree" : "GED",
-            "majors" : "",
-            "dates" : "August 2010 - June 2013",
-            "url" : "https://www.lcps.org/Tuscarora" 
-        }
+            "name": "Tuscarora High School",
+            "location": "Leesburg, VA",
+            "degree": "GED",
+            "majors": "",
+            "dates": "August 2010 - June 2013",
+            "url": "https://www.lcps.org/Tuscarora"
+        },
     ],
-    "onlineCourses" : [
-        {
-            "title" : "Front End Developer Nanodegree",
-            "school" : "Udacity",
-            "dates" : "July 2016 - Present",
-            "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    "onlineCourses": [{
+            "title": "Front End Developer Nanodegree",
+            "school": "Udacity",
+            "dates": "July 2016 - Present",
+            "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
         },
         {
-            "title" : "Full Stack Web Development",
-            "school" : "Udacity",
-            "dates" : "January 2017 - Present",
-            "url" : "https://www.freecodecamp.com/"
+            "title": "Full Stack Web Development",
+            "school": "Udacity",
+            "dates": "January 2017 - Present",
+            "url": "https://www.freecodecamp.com/"
         }
     ]
 };
 
 var work = {
-    "jobs" : [
-        {
-            "employer" : "Milestone Technologies",
-            "title" : "Data Center Technician",
-            "location" : "Loundoun, VA",
-            "dates" : "January 2017 - Present",
-            "description" : "Repair Facebook servers to put them back into produciton and create scripts to automate mundane tasks"
+    "jobs": [{
+            "employer": "Milestone Technologies",
+            "title": "Data Center Technician",
+            "location": "Loundoun, VA",
+            "dates": "January 2017 - Present",
+            "description": "Repair Facebook servers to put them back into produciton and create scripts to automate mundane tasks"
         },
         {
-            "employer" : "Chief-Technologies",
-            "title" : "Intern Junior Systems Engineer",
-            "location" : "Loundoun, VA",
-            "dates" : "June 2013 - December 2016",
-            "description" : "Learn programming principles, go to customer sites to perform network and IP phone installtions, assist engineers with technical tasks"
+            "employer": "Chief-Technologies",
+            "title": "Intern Junior Systems Engineer",
+            "location": "Loundoun, VA",
+            "dates": "June 2013 - December 2016",
+            "description": "Learn programming principles, go to customer sites to perform network and IP phone installtions, assist engineers with technical tasks"
         },
         {
-            "employer" : "Trapollo",
-            "title" : "Contractor - Technical Support / Customer Service Respresentative",
-            "location" : "Sterling, VA",
-            "dates" : "February 2016 - December 2016",
-            "description" : "Perform and explain medical device installations via phone to elderly people and troubleshoot any issues they might encounter"
+            "employer": "Trapollo",
+            "title": "Contractor - Technical Support / Customer Service Respresentative",
+            "location": "Sterling, VA",
+            "dates": "February 2016 - December 2016",
+            "description": "Perform and explain medical device installations via phone to elderly people and troubleshoot any issues they might encounter"
         }
     ]
 };
 
 var projects = {
-    "projects" : [
-        {
-            "title" : "Coming Soon",
-            "dates" : "January 2017",
-            "description" : "Project in progress",
-            "images" : "images/art.jpg"
+    "projects": [{
+            "title": "Coming Soon",
+            "dates": "January 2017",
+            "description": "Project in progress",
+            "images": "images/art.jpg"
         },
         {
-            "title" : "Coming Soon",
-            "dates" : "January 2017",
-            "description" : "Project in progress",
-            "images" : "images/forest.jpg"
+            "title": "Coming Soon",
+            "dates": "January 2017",
+            "description": "Project in progress",
+            "images": "images/forest.jpg"
         },
         {
-            "title" : "Coming Soon",
-            "dates" : "January 2017",
-            "description" : "Project in progress",
-            "images" : "images/yellow.jpg"
+            "title": "Coming Soon",
+            "dates": "January 2017",
+            "description": "Project in progress",
+            "images": "images/yellow.jpg"
         }
     ]
 };
@@ -151,7 +147,7 @@ bio.display = function () {
         "https://twitter.com/jasierragarcia",
         "mailto:jasierra.garcia@gmail.com"
     ];
-    
+
     for (var key in socialList) {
         $(".social-ul").append(socialList[key].replace("#", socialLinks[key]));
     }
@@ -164,7 +160,7 @@ bio.display();
 function displayProjects(project) {
 
     $(".p-title").append(HTMLprojectHead);
-    
+
     for (var i = 0; i < project.length; i++) {
         var formattedTitle = HTMLprojectTitle.replace("%data%", project[i].title);
         var formattedDate = HTMLprojectDates.replace("%data%", project[i].dates);
@@ -202,8 +198,7 @@ education.display = function () {
     $(".e-title").append(HTMLeducationHead);
 
     education = education.schools;
-    courses = education.onlineCourses;
-    
+
     var i = 0;
     for (i = i; i < education.length; i++) {
         var formattedName = HTMLschoolName.replace("%data%", education[i].name);
@@ -215,8 +210,16 @@ education.display = function () {
         $(".e-content").append(formattedMajor);
     }
 
-    // online courses
-    $(".o-title").append(HTMLonlineHead);
+    // online courses component
+    var courses = education.onlineCourses;
+
+    function onlineCourse(course) {
+        for (key in course) {
+            console.log(course[key]);
+        }
+    }
+    onlineCourse(education);
+
 };
 
 education.display();
