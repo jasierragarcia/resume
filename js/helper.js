8bd3cab2286e5e619f07649fa90dac9682bcee48
Juan Sierra <jasierra.garcia@gmail.com>
2017-03-09 23:02:40 -0500

    small changes

--- js/helper.js
+++ js/helper.js
@@ -4,10 +4,10 @@ var HTMLheaderRole = '<div id="role">%data%</div><hr class="hr">';
 
 var HTMLheaderSummary = '<p class="bio">%data%</p>';
 
-var HTMLnavProjects = '<li class="nav-li"><a href="#" id="#" class="nav-link">%data%</a></li>';
-var HTMLnavWork = '<li class="nav-li"><a href="#" id="#" class="nav-link">%data%</a></li>';
-var HTMLnavEdu = '<li class="nav-li"><a href="#" id="#" class="nav-link">%data%</a></li>';
-var HTMLnavContact = '<li class="nav-li"><a href="#" id="#" class="nav-link">%data%</a></li>';
+var HTMLnavProjects = '<li class="nav-li"><a href="#" id="#projects" class="nav-link">%data%</a></li>';
+var HTMLnavWork = '<li class="nav-li"><a href="#" id="#work" class="nav-link">%data%</a></li>';
+var HTMLnavEdu = '<li class="nav-li"><a href="#" id="#education" class="nav-link">%data%</a></li>';
+var HTMLnavContact = '<li class="nav-li"><a href="#" id="#contact" class="nav-link">%data%</a></li>';
 
 var HTMLemail = '<li class="social-li"><a href="#" class="social-link" id="email"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>';
 var HTMLtwitter = '<li class="social-li"><a href="#" class="social-link" id="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>';
@@ -24,23 +24,25 @@ var HTMLprojectImage = '<img src="%data%" class="p-image">';
 var HTMLskills = '<li class="skills">%data%</li>';
 
 var HTMLworkHead = "<h2>Work Experience</h2>";
-var HTMLworkEmployer = '<div href="#" class="employer">%data%</div>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkEmployer = '<div href="#" id="employer">%data%</div>';
+var HTMLworkDates = '<div id="work-date">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p class="work-description"><br>%data%</p>';
+var HTMLworkDescription = '<p id="work-description"><br>%data%</p>';
 
-var HTMLeducationHead = "<h3>Education</h3>";
+var HTMLeducationHead = "<h2>Education</h2>";
 var HTMLschoolName = '<div href="#" class="school-name">%data%</div>';
 var HTMLschoolDates = '<span class="date-text">%data%</span>';
 var HTMLschoolLocation = '<div class="location-text col">%data%</div>';
 var HTMLschoolMajor = '<p>Major: %data%</p>';
 
 
-var HTMLonlineHead = '<h3>Online Courses</h3>';
+var HTMLonlineHead = '<h2>Online Courses</h2>';
 var HTMLonlineSchool = '<a href="#" class="school-name">%data%</a>';
 var HTMLonlineTitle = '<p>%data%</p>';
 var HTMLonlineDates = '<p class="date-text">%data%</p>';
 var HTMLonlineURL = '<a href="#" id="online-URL">%data%</a>';
+
+
 
 
 var HTMLfooter = "<hr id='footer-hr'>";
